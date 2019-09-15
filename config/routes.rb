ce12b7bc17975d12e619e9e56ce9f10a2d890ef1
akino-programmer <noguchi_a7@yahoo.co.jp>
2019-09-16 00:58:45 +0900

    chat-space-automatic-updating-function2

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    
+
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
